10d5832405bf6096b5916a0cab90be579ea80ea4
anukitty <adepunandhu9@gmail.com>
2023-08-10 14:31:22 +0530

    erer3

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
+const {
+    generateRandomText,
+    randomImage,
+    downloadYtVideo,
+    getProfilePic
+} = require('./utils/utils')
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,18 +16,6 @@ app.get('/', (req, res) => {
     const response = { response: botResponse };
     res.json(response);
 });
-function generateRandomText() {
-  const texts = [
-    ' Hello, world!',
-    ' This is a random message.',
-    ' Coding is fun!',
-    ' Explore the possibilities.',
-    ' Randomness at its best.',
-  ];
-
-  const randomIndex = Math.floor(Math.random() * texts.length);
-  return texts[randomIndex];
-}
 
 
 function extractReq(bot){
@@ -49,34 +43,33 @@ maxBot.on('message', async (msg) => {
 
     let text = '';
     let senderMention = '';
+    const availableCommands = [
+        '/start - Introduction about the bot',
+        '/help - Show available commands',
+        '/random_text - Generate random text',
+        '/random_image - Generate random image',
+        '/random_image <width> <height> - Generate random image with width and height',
+        '/download_yt_audio <url> - Download audio from youtube',
+        '/download_yt_video <url> - Download video from youtube',
+        'if you cant recieve my messages : please wake me up here -> https://bot-manager-r4kp.onrender.com/'
+    ];
     console.log(msg.text)
     // If the message is from a group chat, construct the mention
     if (msg.chat.type === 'group' || msg.chat.type === 'supergroup') {
-        const senderId = msg.from.id;
         const senderFirstName = msg.from.username;
 
         senderMention = `@${senderFirstName}`;
     }  
     // Check if userCommand starts with certain strings
     if (userCommand.startsWith('/start') || userCommand.startsWith('start')) {
-        text = `${senderMention} Introducing Maxpark, the innovative bot designed by cuber-dev! Maxpark is your ultimate YouTube companion, adept at effortlessly downloading both videos and audio content. With its seamless integration, you can now enjoy your favorite media offline, thanks to Maxpark's exceptional capabilities. Say goodbye to buffering and hello to convenience, as Maxpark simplifies your entertainment experience. i can help you with downloading youtube videos and audios and i am also able to genearte random texts , images. i am still in development , if you have any questions or suggestions ,please feel free to contact my author at github , telegram : ID : cuber-dev. i hope you enjoy using me. thanks for using me. /help for help. if you cant recive my messages : please wake me up here https://bot-manager-r4kp.onrender.com/`
+        text = `${senderMention} Introducing Maxpark, the innovative bot designed by cuber-dev! Maxpark is your ultimate YouTube companion, adept at effortlessly downloading both videos and audio content. With its seamless integration, you can now enjoy your favorite media offline, thanks to Maxpark's exceptional capabilities. Say goodbye to buffering and hello to convenience, as Maxpark simplifies your entertainment experience. i can help you with downloading youtube videos and audios and i am also able to genearte random texts , images. i am still in development , if you have any questions or suggestions ,please feel free to contact my author at github , telegram : ID : cuber-dev. i hope you enjoy using me. thanks for using me. /help for help. if you cant recieve my messages : please wake me up here -> https://bot-manager-r4kp.onrender.com/`
         
         sendMsg(maxBot, chatId, text);
     } else if (userCommand.startsWith('/help') || userCommand.startsWith('help')) {
-        const availableCommands = [
-                        '/start - Introduction about the bot',
-                        '/help - Show available commands',
-                        '/random_text - Generate random text',
-                        '/random_image - Generate random image',
-                        '/random_image <width> <height> - Generate random image with width and height',
-                        '/download_yt_audio <url> - Download audio from youtube',
-                        '/download_yt_video <url> - Download video from youtube',
-                        'if you cant recive my messages : please wake me up here https://bot-manager-r4kp.onrender.com/'
-                    ];
         const helpText = `${senderMention} Available commands:\n${availableCommands.join('\n')}`;
         sendMsg(maxBot, chatId, helpText);
     } else if (userCommand.startsWith('/random_text')) {
-        text = senderMention + generateRandomText();
+        text = senderMention + " " + generateRandomText();
         sendMsg(maxBot, chatId, text);
     } else if (userCommand.startsWith('/random_image')) {
         sendMsg(maxBot, chatId, `${senderMention} Please wait, generating image...`);
@@ -84,7 +77,7 @@ maxBot.on('message', async (msg) => {
             maxBot.sendPhoto(chatId, randomImage(msg.text));
         },100)
     } else if (userCommand.startsWith('/download_yt')) {
-        await downloadYtVideo(userCommand, userMessage, chatId, senderMention);
+        await downloadYtVideo(maxBot,userCommand, userMessage, chatId, senderMention);
     }
 
     if (msg.new_chat_members) {
@@ -103,49 +96,6 @@ maxBot.on('message', async (msg) => {
 });
 
 
-async function downloadYtVideo(userCommand,userMessage,chatId ,senderMention){
-    const URI = `https://yt-vd-bot.onrender.com/download`
-        
-        if(!userMessage){
-            maxBot.sendMessage(chatId, `${senderMention} Please enter a youtube video url`)
-            return 
-        } 
-
-        const ytUrl = userMessage
-        const response = await verifyYtUrl(ytUrl)
-        if(!response){
-            maxBot.sendMessage(chatId, `${senderMention} Please enter a valid youtube url`)
-            return
-        }
-            if (userCommand.startsWith('/download_yt_audio')) {
-                try {
-                    maxBot.sendMessage(chatId, `${senderMention} Audio is downloading, please wait...`)
-                    maxBot.sendDocument(chatId, `${URI}/audio?url=${ytUrl}`);
-                } catch (error) {
-                    maxBot.sendMessage(chatId,`${senderMention} Error downloading audio (500)`);
-                }
-            } else if (userCommand.startsWith('/download_yt_video')) {
-                try {
-                    maxBot.sendMessage(chatId, `${senderMention} video is downloading, please wait...`)
-                    maxBot.sendDocument(chatId, `${URI}/video?url=${ytUrl}&pixels=720`);
-
-                } catch (error) {
-                    maxBot.sendMessage(chatId,`${senderMention} Error downloading video (500)`);
-                }
-            }
-}
-function randomImage(text){
-    text = text.replace('@Maxpark09_bot','')
-    const w = text.split(/\s+/i)[1]
-    const h = text.split(/\s+/i)[2]
-    if(w && h){
-        const image = `https://unsplash.it/${w}/${h}`
-        return image;
-    }
-    const image = Math.floor(Math.random() * 5000);
-    const randomImage = `https://picsum.photos/${image}`;
-    return randomImage;
-}
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
---
+++ utils/utils.js
@@ -0,0 +1,145 @@
+let formatLabels  = [
+    "Identicon",
+    "Botts",
+    "Pixel-art",
+    "Pixel-art-neutral",
+    "Adventure",
+    "Thumb",
+    'Icon',
+    'Adventurer-neutral',
+    'Avaatars',
+    'Avaatars-neutral',
+    'Big-ears',
+    'Big-ears-neutral',
+    'Big-smile',
+    'Botts-neutral',
+    'Croodles',
+    'Croodles-neutral',
+    'Fun-emoji',
+    'Lorelei',
+    'Lorelei-neutral',
+    'Shapes'
+    ];
+let formatPaths = [
+    '5.x/identicon',
+    '5.x/bottts',
+    '4.x/pixel-art',
+    '5.x/pixel-art-neutral',
+    '5.x/adventurer',
+    '5.x/thumbs',
+    '5.x/icons',
+    '5.x/adventurer-neutral',
+    '5.x/avataaars',
+    '5.x/avataaars-neutral',
+    '5.x/big-ears',
+    '5.x/big-ears-neutral',
+    '5.x/big-smile',
+    '5.x/bottts-neutral',
+    '5.x/croodles',
+    '5.x/croodles-neutral',
+    '5.x/fun-emoji',
+    '5.x/lorelei',
+    '5.x/lorelei-neutral',
+    '5.x/shapes'
+  ];
+
+let formatType = 'jpg';
+let newPath = 0,newLabel = 0
+async function getProfilePic(username,imgType,labelType,pathType){
+    formatType = imgType || formatType;
+    formatPaths.forEach((path,i) => {
+        if(path.includes(pathType)){
+            newPath = i;
+        }
+        if(formatLabels[i].includes(labelType)){
+            newLabel = i;
+        }
+    })
+    const url =  `https://api.dicebear.com/${formatPaths[i]}/${formatType}?seed=${username}`;
+    return url     
+  }
+
+
+
+  function generateRandomText() {
+    const texts = [
+      'Hello, world!',
+      'This is a random message.',
+      'Coding is fun!',
+      'Explore the possibilities.',
+      'Randomness at its best.',
+    ];
+  
+    const randomIndex = Math.floor(Math.random() * texts.length);
+    return texts[randomIndex];
+}
+  
+const { verifyYtUrl } = require('../bots/max');
+
+async function downloadYtVideo(bot,userCommand,userMessage,chatId ,senderMention){
+    const URI = `https://yt-vd-bot.onrender.com/download`
+        
+        if(!userMessage){
+            bot.sendMessage(chatId, `${senderMention} Please enter a youtube video url ,
+            ex : /download_yt_video https://www.youtube.com/watch?v=XXXX`)
+            return 
+        } 
+
+        const ytUrl = userMessage
+        const response = await verifyYtUrl(ytUrl)
+        if(!response){
+            bot.sendMessage(chatId, `${senderMention} Please enter a valid youtube url,
+            ex : /download_yt_video https://www.youtube.com/watch?v=XXXX`)
+            return
+        }
+            if (userCommand.startsWith('/download_yt_audio')) {
+                try {
+                    bot.sendMessage(chatId, `${senderMention} Audio is downloading, please wait...`)
+                    const response = await bot.sendDocument(chatId, `${URI}/audio?url=${ytUrl}`);
+
+                    if (response) {
+                        bot.sendMessage(chatId, `${senderMention} Audio is downloaded successfully`);
+                    } else {
+                        bot.sendMessage(chatId, `${senderMention} There was an issue downloading the audio.`);
+                    }
+                }
+                 catch (error) {
+                    bot.sendMessage(chatId,`${senderMention} Error downloading audio (500)`);
+                }
+            } else if (userCommand.startsWith('/download_yt_video')) {
+                try {
+                    bot.sendMessage(chatId, `${senderMention} video is downloading, please wait...`)
+                    const response = await bot.sendDocument(chatId, `${URI}/video?url=${ytUrl}&pixels=720`);
+                   
+                    if (response) {
+                        bot.sendMessage(chatId, `${senderMention} Video is downloaded successfully`);
+                    } else {
+                        bot.sendMessage(chatId, `${senderMention} There was an issue downloading the video.`);
+                    }
+                } catch (error) {
+                    bot.sendMessage(chatId,`${senderMention} Error downloading video (500)`);
+                }
+            }
+}
+function randomImage(text){
+    text = text.replace('@Maxpark09_bot','')
+    const w = text.split(/\s+/i)[1]
+    const h = text.split(/\s+/i)[2]
+    if(w && h){
+        const image = `https://unsplash.it/${w}/${h}`
+        return image;
+    }
+    const image = Math.floor(Math.random() * 5000);
+    const randomImage = `https://picsum.photos/${image}`;
+    return randomImage;
+}
+
+
+
+
+module.exports = {
+    randomImage,
+    downloadYtVideo,
+    getProfilePic,
+    generateRandomText
+}
